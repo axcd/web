4edd16d5a8f9ca5fb462bba143b674b39ba837df
axcd <maoyuyin@126.com>
2019-05-27 22:59:54 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-var result = /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent);
+var result = /Mobile/i.test(navigator.userAgent);
 
 if(!result){
   location.href="https://axcd.github.io/web/0";
