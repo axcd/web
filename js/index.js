29c020bc57d3d23422444c181877220145dc07d2
axcd <maoyuyin@126.com>
2019-05-27 15:09:21 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
+
 var str = navigator.userAgent;
 var re = new RegExp("Mobile");
 
 var result = re.test(str);
 
-if(result){
+if(!result){
   location.href="https://axcd.github.io/web/0";
-}else{
-  document.write("<h1>此网页只支持手机显示</h1>");
 }
