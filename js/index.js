f7fb946d93c56619c2334e65dcb2a14da307c1a5
axcd <maoyuyin@126.com>
2019-05-27 12:34:55 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,41 +1,10 @@
-$(功能(){
-    $("#添加").点击(功能(){
-         $.阿贾克斯({
-高速缓存: "假的",
-类型: "岗",
-URL: "http://www.huaba.ml/Home/index/check",
-数据: {
-名字，姓名: $("#exampleinputemail 1").瓦尔(),
-帕斯: $("#exampleinputpassword 1").瓦尔(),
-                   },
-             成功: 功能 (数据) {
-                   如果(数据)=="1"){
-                       窗户.位置="http://www.huaba.ml/Home/index/add";
-                   }其他{
-                       $("#mymode").模态("肘节");
-                   }
-             },
-         });
-    });
-    $("#BTN").点击 (功能 () {
-         窗户.位置="http://www.huaba.ml/Home/index/search/where/"+$("#我的输入").瓦尔();
-    });
-    $("#登录").点击(功能(){
-         $.阿贾克斯({
-高速缓存: "假的",
-类型: "岗",
-URL: "http://www.huaba.ml/Home/index/check",
-数据: {
-名字，姓名: $("#exampleinputemail 1").瓦尔(),
-帕斯: $("#exampleinputpassword 1").瓦尔(),
-                   },
-             成功: 功能 (数据) {
-                   如果(数据)=="1"){
-                       窗户.位置="http://www.huaba.ml/Home/index/add";
-                   }其他{
-                       $("#mymode").模态("肘节");
-                   }
-             },
-         });
-    });
-})
+var str = navigator.userAgent;
+var re = new RegExp("Mobile");
+
+var result = re.test(str);
+
+if(result){
+  location.href="https://axcd.github.io/web/0";
+}else{
+  document.write("<h1>这是一个手机网页</h1>");
+}
