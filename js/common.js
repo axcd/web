ee0ec685250475f1da795e8628b0913260f5f1ff
axcd <maoyuyin@126.com>
2019-05-29 22:03:55 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -2,6 +2,6 @@
 var result = /(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i.test(navigator.userAgent);
 
 if(!result){
-  location.href="https://axcd.github.io/web/0";
+  location.href="https://axcd.github.io/web/403";
 }
 
