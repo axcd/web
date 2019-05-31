ef7e9da973455a9402e9f7abd5d6993151620ee5
axcd <maoyuyin@126.com>
2019-05-31 21:49:40 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -5,9 +5,12 @@ if(!result){
   location.href = "https://axcd.github.io/web/403";
 };
 
-x = document.getElementsByTagName('body')[0];
-window.alert(x);
-x.clientWidth = screen.width+"px";
-x.clientHeight = screen.height+"px";
+function load(){
 
-window.alert(x.clientHeight+"px");
+  x = document.getElementsByTagName('body')[0];
+  window.alert(x);
+  x.clientWidth = screen.width+"px";
+  x.clientHeight = screen.height+"px";
+
+  window.alert(x.clientHeight+"px");
+}
