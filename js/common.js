f857d0c9d7dc070fc7676d392242fbf9140f882a
axcd <maoyuyin@126.com>
2019-05-31 11:25:16 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -5,8 +5,3 @@ var result = /(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|sym
 if(!result){
   location.href="https://axcd.github.io/web/403";
 };
-
-x = document.getElementsByTagName('body')[0];
-
-x.style.width = screen.width;
-x.style.height = screen.height;
