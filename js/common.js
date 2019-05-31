682fbbfdbd8b1c8bff5756ed577a697882066886
axcd <maoyuyin@126.com>
2019-05-31 22:21:54 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -7,8 +7,8 @@ if(!result){
 
 function load(){
 
-  document.body.clientWidth = screen.width-2;
-  document.body.clientHeight = screen.height-2;
+  document.body.clientWidth = screen.width;
+  document.body.clientHeight = screen.height;
 
   window.alert(x.clientWidth);
   window.alert(x.height);
