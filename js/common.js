3a4b53867888a2f92edaf81f5f6022ca81c04ba3
axcd <maoyuyin@126.com>
2019-05-31 10:57:03 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@ if(!result){
   location.href="https://axcd.github.io/web/403";
 };
 
-x=document.getElementsByTagName('body')[0];
+x = document.getElementsByTagName('body')[0];
 
 x.style.width = screen.width;
 x.style.height = screen.height;
