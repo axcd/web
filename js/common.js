78da11efdcd9ffc15e0beaf19f17da7557e5a5d3
axcd <maoyuyin@126.com>
2019-05-31 09:53:04 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 
-window.alert("123");
+window.alert(5 + 6);
+
 var result = /(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i.test(navigator.userAgent);
 
 if(!result){
